3d4d0fd9f6f9dcce782a3383b4df031cd4c3652a
Margaret Star <imdrumcan@gmail.com>
2018-05-05 01:33:01 +0200

    initial version of a tiny roleplaying utility, built for pathfinder games over discord.

---
+++ client.rb
@@ -0,0 +1,75 @@
+# a tiny client for hal to roleplay as npcs with.
+# expects a .json file with character data called 'characters.json'
+# character data can potentially include tokens and client_ids.
+require 'socket'
+require 'json'
+require 'readline'
+
+HOST = 'localhost'
+PORT = 45790
+SHOW_CHARACTER_BIOS = true
+
+def read_json(file)
+  File.open(file, 'r') { |f| JSON.parse(f.read, symbolize_names: true) }
+end
+
+secrets    = read_json('secrets.json')
+characters = read_json('characters.json')
+AUTOCOMPLETION_LIST = characters.keys.sort
+
+server = TCPSocket.open(HOST, PORT)
+at_exit { puts 'bye-bye!'; server.close }
+
+# don't append a character after autocompletion
+Readline.completion_append_character = nil
+# automcompletion looks at the entire line, not just the last word
+Readline.completer_word_break_characters = ''
+# enable autocompletion of character names
+Readline.completion_proc = proc do |s|
+  AUTOCOMPLETION_LIST.grep(/#{Regexp.escape(s)}/)
+end
+
+# convenience method for using readline with history
+# also allows safe exiting out of program at any input
+def read(sym: '> ')
+  input = Readline.readline(sym, true)
+  exit if input == '/exit' || input == ':q'
+  input
+end
+
+loop do
+  # write npc selection menu with bios
+  if SHOW_CHARACTER_BIOS
+    characters.keys.each do |name|
+      puts "\e[34m#{name}\e[0m:"
+      puts characters[name][:bio]
+      puts
+    end
+  end
+
+  puts "Select NPC by name: "
+  selected = ''
+  until characters.keys.include?(selected = read.to_sym)
+    puts "Invalid character name. Please try again:"
+  end
+
+  puts "\nEnter message as #{selected}:"
+  message = read
+  next if message == '/back'
+  puts
+
+  # build json to send to server
+  # use character-specific token and client_id, if supplied.
+  data = {
+    name: selected,
+    token: characters[selected][:token] || secrets[:token],
+    client_id: characters[selected][:client_id] || secrets[:client_id],
+    channel_id: secrets[:channel_id],
+    image: characters[selected][:portrait],
+    message: message
+  }.to_json
+
+  server.puts(data)
+  puts '=' * 80
+  puts
+end
---
+++ server.rb
@@ -0,0 +1,42 @@
+require 'discordrb'
+require 'socket'
+require 'json'
+
+server = TCPServer.open(45790)
+
+bots = {}
+
+# wait for client to connect
+while client = server.accept
+  # client has sent a message
+  while received = client.gets
+    data = JSON.parse(received, symbolize_names: true)
+
+    # just looks cleaner further down.
+    character = data[:name].to_sym
+    channel_id = data[:channel_id]
+    message = data[:message]
+    image = data[:image]
+
+    # create bot with this name if it doesn't exist already
+    unless bots[character]
+      token = data[:token]
+      id = data[:client_id]
+
+      bots[character] = Discordrb::Bot.new(token: token, client_id: id)
+      puts "bot '#{character}' created"
+
+      # start specified bot
+      bots[character].run :async
+    end
+
+    # send message to discord; post an image first if one was specified
+    if image
+      File.open(image, 'r') do |f|
+        bots[character].send_file(channel_id, f)
+      end
+    end
+    bots[character].send_message(channel_id, "**#{character}:** #{message}")
+  end
+end
+
